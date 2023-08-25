c40490ae48ec21004ece184b4f101caa0d925762
katesyvash795 <126354194+katesyvash795@users.noreply.github.com>
2023-08-25 16:23:21 +0300

    k

--- src/components/transactionHystory/TransactionHystory.styled.js
+++ src/components/transactionHystory/TransactionHystory.styled.js
@@ -3,19 +3,15 @@ export const Table = styled.table`
   width: 300px;
   border-collapse: collapse;
 `;
-
 export const TableHeader = styled.th`
   background-color: blue;
   color: white;
   padding: 8px;
 `;
-
 export const TableRow = styled.tr`
-
   background-color: ${props => (props.$even ? '#f2f2f2' : 'white')};
 `;
-
 export const TableCell = styled.td`
   padding: 8px;
-  text-align:center;
+  text-align: center;
 `;
